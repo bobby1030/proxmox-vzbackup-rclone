dd897cad5b2cc8b458747f552a9000b1f67e14ca
BobbyHo <angela.bobby@gmail.com>
2022-01-29 21:57:33 +0800

    Modify path to fit production environment

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -2,10 +2,10 @@
 # Source: https://github.com/TheRealAlexV/proxmox-vzbackup-rclone
 # ./vzbackup-rclone.sh rehydrate YYYY/MM/DD file_name_encrypted.bin
 ############ /START CONFIG
-dumpdir="/mnt/pve/pvebackups01/dump" # Set this to where your vzdump files are stored
+dumpdir="/mnt/usbdriv/pvedump" # Set this to where your vzdump files are stored
 MAX_AGE=3 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
-REMOTE_NAME=gd-backup_crypt
-SUB_PATH=/proxmox/
+REMOTE_NAME=ntu-onedrive
+SUB_PATH=/pvedump
 CONFIG_PATH=/root/.config/rclone/rclone.conf
 ############ /END CONFIG
 
